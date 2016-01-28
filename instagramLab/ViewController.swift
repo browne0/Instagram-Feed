2409fc39ef11d021406c19ea57b99c3c2828a41e
Malik <browne0@purdue.edu>
2016-01-28 13:33:16 -0500

    Initial Commit

---
+++ instagramLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramLab
+//
+//  Created by Malik Browne on 1/28/16.
+//  Copyright © 2016 Malik Browne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
